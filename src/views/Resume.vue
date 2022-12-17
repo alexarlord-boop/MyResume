8ce843a68fcf03c6efb56b0e2549a5a5c69b7528
alexarlord-boop <rezerw0.1@yandex.ru>
2022-12-17 10:46:13 +0200

    RESUME-2022 Refactored tag classes

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,31 +1,32 @@
 <template>
-  <div class="line-container">
-
-    <svg viewBox="0 0 143 176" fill="none" preserveAspectRatio="xMidYMax meet">
-      <path id="line"
-            d="M4 7.50008C25.3333 22.0001 156.217 69.4115 145 18.0001C133 -37 23 119.5 19.5 75.5001C16.7 40.3001 96.6667 87.1667 133 115C162 3.00008 -9 123.5 31 147.5C50.3333 166.167 89.1 192.3 89.5 147.5C89.9 102.7 68 112.167 57 122.5L13 164.5"
-            stroke-linecap="round" stroke-linejoin="round" stroke-opacity="0.9" stroke="#ffd700" stroke-width="11"/>
-    </svg>
-
-  </div>
-
-  <div class="cards-wrap">
-
-    <div id="title1" class="parallax-title">About</div>
-    <CardInfo v-for="(card, index) in cardsAbout" :card-id="'card'+index">{{ card.innerText }}</CardInfo>
-
-    <div class="closer-cards">
-      <CardInfo v-for="card in cardsStack">{{card.innerText}}</CardInfo>
+  <div>
+    <div class="line-container">
+      <svg viewBox="0 0 143 176" fill="none" preserveAspectRatio="xMidYMax meet">
+        <path id="line"
+              d="M4 7.50008C25.3333 22.0001 156.217 69.4115 145 18.0001C133 -37 23 119.5 19.5 75.5001C16.7 40.3001 96.6667 87.1667 133 115C162 3.00008 -9 123.5 31 147.5C50.3333 166.167 89.1 192.3 89.5 147.5C89.9 102.7 68 112.167 57 122.5L13 164.5"
+              stroke-linecap="round" stroke-linejoin="round" stroke-opacity="0.9" stroke="#ffd700" stroke-width="11"/>
+      </svg>
     </div>
 
-    <div class="closer-cards">
-      <CardInfo v-for="card in cardsLink"><a :href="card.lnk">{{card.innerText}}</a></CardInfo>
+    <div class="cards-wrap">
+
+      <div id="title" class="title-parallax">About</div>
+      <CardInfo v-for="(card, index) in cardsAbout" :card-id="'card'+index">{{ card.innerText }}</CardInfo>
+
+      <div class="cards-group">
+        <CardInfo v-for="card in cardsStack">{{ card.innerText }}</CardInfo>
+      </div>
+
+      <div class="cards-group">
+        <CardInfo v-for="card in cardsLink"><a :href="card.lnk">{{ card.innerText }}</a></CardInfo>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script>
+//TODO: -scroll functions optimisation
+//TODO: -refactor mobile version via @media
 import CardInfo from "@/components/CardInfo.vue";
 
 export default {
@@ -39,14 +40,13 @@ export default {
       cardsAbout: [{innerText: "I am an IT student, seeking for improvement in computer science field."},
         {innerText: "I am passionate about software development and making product looks AMAZING!"},
         {innerText: "I've worked as a full-stack developer for 1.5 years!"},
-        ],
+      ],
       cardsStack: [{innerText: 'git'}, {innerText: 'Java Spring'}, {innerText: 'MS SQL'},
         {innerText: 'PostgreSQL'}, {innerText: 'JavaScript'}, {innerText: 'CSS/HTML'},
         {innerText: 'Vue.js'}, {innerText: 'Node.js'},
         {innerText: 'C#'}, {innerText: 'Python'}, {innerText: 'Linux'}],
       cardsLink: [{innerText: 'github', lnk: 'https://github.com/alexarlord-boop/alexarlord-boop/blob/main/README.md'},
         {innerText: 'VK', lnk: ''}],
-      percentageArray: null
     }
   },
   methods: {
@@ -63,16 +63,16 @@ export default {
     MoveTitleParallax() {
       let scrollPerc = this.GetScrolledPercentage();
 
-      document.getElementById('title1').style.top = String(30 + this.GetScrolledPercentage() * 300) + "px";
+      document.getElementById('title').style.top = String(30 + this.GetScrolledPercentage() * 300) + "px";
 
       if (scrollPerc <= 0.43) {
-        document.getElementById('title1').innerText = 'About';
+        document.getElementById('title').innerText = 'About';
       }
       if (scrollPerc > 0.43 && scrollPerc <= 0.80) {
-        document.getElementById('title1').innerText = 'Stack';
+        document.getElementById('title').innerText = 'Stack';
       }
       if (scrollPerc > 0.80) {
-        document.getElementById('title1').innerText = 'Links';
+        document.getElementById('title').innerText = 'Links';
       }
     }
   },
@@ -95,14 +95,13 @@ export default {
 .cards-wrap {
   height: 240vh;
   margin: 0;
-
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
 }
 
-.closer-cards {
+.cards-group {
   width: 400px;
   display: flex;
   flex-wrap: wrap;
@@ -124,7 +123,7 @@ svg {
   display: inline-block;
 }
 
-.parallax-title {
+.title-parallax {
   position: fixed;
   top: 30px;
   left: 100px;
