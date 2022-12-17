6bf7faa6551120719c8c8085e5fbb4e07b5d5405
alexarlord-boop <rezerw0.1@yandex.ru>
2022-12-17 22:35:19 +0200

    RESUME-2022 Added icon style (desktop)

--- src/components/ResumeLinks.vue
+++ src/components/ResumeLinks.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="links-group">
-    <IconGit class="icon-lnk" @click="GoToLnk(cardsLink[0].lnk)"></IconGit>
-    <IconVK class="icon-lnk" @click="GoToLnk(cardsLink[1].lnk)"></IconVK>
+    <IconGit class="icon-lnk cross-shape" @click="GoToLnk(cardsLink[0].lnk)"></IconGit>
+    <IconVK class="icon-lnk circle-shape" @click="GoToLnk(cardsLink[1].lnk)"></IconVK>
     <IconLNKD class="icon-lnk" @click="GoToLnk(cardsLink[2].lnk)"></IconLNKD>
   </div>
 </template>
@@ -14,10 +14,13 @@ import IconLNKD from "@/components/icons/IconLNKD.vue";
 export default {
   name: "ResumeLinks",
   components: {IconLNKD, IconVK, IconGit},
-  data(){
+  data() {
     return {
       cardsLink: [{innerText: 'github', lnk: 'https://github.com/alexarlord-boop'},
-        {innerText: 'VK', lnk: 'https://vk.com/alexarlord'}, {innerText: 'LinkedIn', lnk: 'https://www.linkedin.com/in/aleksandr-petrunin-b4b720259/'}]
+        {innerText: 'VK', lnk: 'https://vk.com/alexarlord'}, {
+          innerText: 'LinkedIn',
+          lnk: 'https://www.linkedin.com/in/aleksandr-petrunin-b4b720259/'
+        }]
     }
   },
   methods: {
@@ -36,13 +39,26 @@ export default {
   flex-direction: row;
   justify-content: space-around;
 }
+
 .icon-lnk {
   width: 50px;
   height: 50px;
   cursor: pointer;
   transition: 0.1s ease-out;
-
 }
+
+.cross-shape {
+  clip-path: polygon(0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%);
+}
+
+.trapezoid-shape {
+  clip-path: polygon(20% 0%, 80% 0%, 100% 100%, 0% 100%);
+}
+
+.circle-shape {
+  clip-path: circle(50% at 50% 50%);
+}
+
 @media screen and (min-width: 601px) {
   .links-group {
     left: 100px;
@@ -50,15 +66,18 @@ export default {
   .icon-lnk {
     width: 150px;
     height: 150px;
+
     &:hover {
-      transform: scale(1.1);
+      z-index: 500;
+      transform: scale(1.3) rotate(10deg);
       background-color: blueviolet;
+
     }
   }
 }
 
 @media screen and (min-width: 325px) and (max-width: 600px) {
-  .links-group{
+  .links-group {
     display: flex;
     justify-content: space-between;
     top: -50px;
